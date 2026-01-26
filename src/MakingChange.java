0c50e144ae290b4a3d85639a5ab14aa40f302021
hannahbernthal <hannah.bernthal@menloschool.org>
2026-01-25 20:31:23 -0800

    V1 done

--- src/MakingChange.java
+++ src/MakingChange.java
@@ -3,15 +3,26 @@
  * given a set of coins, how many ways can you make change for a target amount?
  *
  * @author Zach Blick
- * @author [YOUR NAME HERE]
+ * @author Hannah Bernthal
  */
 
 public class MakingChange {
-    /**
-     * TODO: Complete this function, countWays(), to return the number of ways to make change
-     *  for any given total with any given set of coins.
-     */
     public static long countWays(int target, int[] coins) {
+        long[][] memoizationTable = new long[target + 1][coins.length];
+
+        int index = 0;
+        if (target == 0) {
+            return 1;
+        }
+        if (target < 0) {
+            return 0;
+        }
+        if (index == coins.length) {
+            return 0;
+        }
+
+
+
         return 0;
     }
 }
