e0a07f76302d5063351ba35c51fa2a98d88521ae
hannahbernthal <hannah.bernthal@menloschool.org>
2026-01-31 11:42:50 -0800

    Done

--- src/MakingChange.java
+++ src/MakingChange.java
@@ -7,22 +7,77 @@
  */
 
 public class MakingChange {
+    public static long[][] table;
     public static long countWays(int target, int[] coins) {
-        long[][] memoizationTable = new long[target + 1][coins.length];
+        table = new long[coins.length][target + 1];
+//        // Memoization
+//        return memoHelper(target, coins, coins.length - 1);
 
-        int index = 0;
-        if (target == 0) {
-            return 1;
-        }
-        if (target < 0) {
-            return 0;
-        }
-        if (index == coins.length) {
-            return 0;
+        // Tabulation
+
+        // Fill in the 0th row with ones.
+        for (int i = 0; i < coins.length; i++) {
+            table[i][0] = 1;
         }
 
+        for (int i = 0; i < coins.length; i++) {
+            for (int j = 1; j <= target; j++) {
+                // Represents the spot that is the coin's value over to the left.
+                long leftSpot = j - coins[i];
 
+                // Represents the spot above.
+                long upSpot = i - 1;
 
-        return 0;
+                // Check if the left spot is in bounds. If it isn't you add 0 from that part.
+                if (leftSpot < 0) {
+                    leftSpot = 0;
+                }
+                else {
+                    // Otherwise, you add the value in that spot.
+                    leftSpot = table[i][(int) leftSpot];
+                }
+
+                // Same as what you did for left, just looking up instead.
+                if (upSpot < 0) {
+                    upSpot = 0;
+                }
+                else {
+                    upSpot = table[(int) upSpot][j];
+                }
+
+                // Fill in the table with the summed values.
+                table[i][j] = leftSpot + upSpot;
+            }
+        }
+
+        // Return the bottom right value because that represents the total possible ways.
+        return table[coins.length - 1][target];
+
     }
+
+//    public static long memoHelper(int target, int[] coins, int index) {
+//        if (target == 0) {
+//            return 1;
+//        }
+//        if (target < 0) {
+//            return 0;
+//        }
+//        if (index < 0) {
+//            return 0;
+//        }
+//
+//        if (table[target][index] != 0) {
+//            return table[target][index];
+//        }
+//
+//        long answer = 0;
+//        answer += helper(target - coins[index], coins, index);
+//        table[index][target] = answer;
+//
+//        answer += helper(target, coins, index - 1);
+//        table[index][target] = answer;
+//
+//        // Return included ways + excluded ways.
+//        return answer;
+//    }
 }
