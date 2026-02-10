569b408c19ab6371852cb7a93c7f341855b9bf01
hannahbernthal <hannah.bernthal@menloschool.org>
2026-02-10 13:03:47 -0800

    done with everything

--- src/MakingChange.java
+++ src/MakingChange.java
@@ -10,50 +10,50 @@ public class MakingChange {
     public static long[][] table;
     public static long countWays(int target, int[] coins) {
         table = new long[coins.length][target + 1];
-        // Memoization
-        return memoHelper(target, coins, coins.length - 1);
+//        // Memoization
+//        return memoHelper(target, coins, coins.length - 1);
 
-//        // Tabulation
-//
-//        // Fill in the 0th row with ones.
-//        for (int i = 0; i < coins.length; i++) {
-//            table[i][0] = 1;
-//        }
-//
-//        for (int i = 0; i < coins.length; i++) {
-//            for (int j = 1; j <= target; j++) {
-//                // Represents the spot that is the coin's value over to the left.
-//                long leftSpot = j - coins[i];
-//
-//                // Represents the spot above.
-//                long upSpot = i - 1;
-//
-//                long total = 0;
-//
-//                // Check if the left spot is in bounds. If it isn't you add 0 from that part.
-//                if (leftSpot < 0) {
-//                    leftSpot = 0;
-//                }
-//                else {
-//                    // Otherwise, you add the value in that spot.
-//                    total += table[i][(int) leftSpot];
-//                }
-//
-//                // Same as what you did for left, just looking up instead.
-//                if (upSpot < 0) {
-//                    upSpot = 0;
-//                }
-//                else {
-//                    total += table[(int) upSpot][j];
-//                }
-//
-//                // Fill in the table with the summed values.
-//                table[i][j] = total;
-//            }
-//        }
-//
-//        // Return the bottom right value because that represents the total possible ways.
-//        return table[coins.length - 1][target];
+        // Tabulation
+
+        // Fill in the 0th row with ones.
+        for (int i = 0; i < coins.length; i++) {
+            table[i][0] = 1;
+        }
+
+        for (int i = 0; i < coins.length; i++) {
+            for (int j = 1; j <= target; j++) {
+                // Represents the spot that is the coin's value over to the left.
+                long leftSpot = j - coins[i];
+
+                // Represents the spot above.
+                long upSpot = i - 1;
+
+                long total = 0;
+
+                // Check if the left spot is in bounds. If it isn't you add 0 from that part.
+                if (leftSpot < 0) {
+                    leftSpot = 0;
+                }
+                else {
+                    // Otherwise, you add the value in that spot.
+                    total += table[i][(int) leftSpot];
+                }
+
+                // Same as what you did for left, just looking up instead.
+                if (upSpot < 0) {
+                    upSpot = 0;
+                }
+                else {
+                    total += table[(int) upSpot][j];
+                }
+
+                // Fill in the table with the summed values.
+                table[i][j] = total;
+            }
+        }
+
+        // Return the bottom right value because that represents the total possible ways.
+        return table[coins.length - 1][target];
 
     }
 
